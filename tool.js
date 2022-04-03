bf526a05fa7c79740f9a0589c7dd5d0aeccd4333
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-04-03 21:30:11 +0800

    Create tool.js

---
+++ tool.js
@@ -0,0 +1,26 @@
+function MirrorProtection(url) {
+	var uri = window.location.href;
+	console.log();
+	if (uri.includes(url)) {
+	console.log('网址验证成功');
+	}else{
+	console.log('发现镜像站！！！');
+	window.location.href='https://www.yt-blog.top/MirrorProtection/?url='+url+'&uri='+uri;
+}
+};
+//判断是国家公祭日12.13，加载css样式。
+function Memorial(){
+    var date=new Date();
+    if(date.getMonth()===2&&date.getDate()===31){
+        document.getElementsByTagName('head')[0].innerHTML += '<style type="text/css">html {filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);-webkit-filter: grayscale(1);}<style>'
+}};
+//判断是春节，显示小灯笼
+function lantern(){
+    var date=new Date();
+    if(date.getMonth()<2){
+        document.getElementsByTagName('head')[0].innerHTML += '<script src="https://api.vvhan.com/api/denglong"><script>'
+}};
+        var head = document.getElementsByTagName('head')[0] ;
+        head.appendChild(styleNode);
+    }
+})();
