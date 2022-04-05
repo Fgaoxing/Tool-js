692c7edc03128ddfcb40b34aa9237b50cde60c30
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-04-05 17:06:07 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -1,3 +1,19 @@
+
+//url替换
+function UrlChange(head,footer,change,out) {
+	for (n in document.getElementsByTagName('a')){
+		if (document.getElementsByTagName('a')[Number(n)].href.indexOf('javascript:') < 0){ 
+		if (document.getElementsByTagName('a')[Number(n)].href.indexOf(out) < 0){ 
+		if(change){
+		document.getElementsByTagName('a')[Number(n)].href=head+encodeURIComponent(document.getElementsByTagName('a')[Number(n)].href)+footer
+		}else{
+		document.getElementsByTagName('a')[Number(n)].href=head+document.getElementsByTagName('a')[Number(n)].href+footer
+		}
+		}
+		}
+}
+};
+
 function MirrorProtection(url) {
 	var uri = window.location.protocol+'//'+document.domain;
 	console.log();
