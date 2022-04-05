cbbe36436e4ab0ae4d9c1fa5a04ed46b0d7c1809
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-04-05 17:36:54 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -24,6 +24,15 @@ function MirrorProtection(url) {
 	window.location.href='https://www.yt-blog.top/MirrorProtection/?url='+url+'/'+window.location.pathname+'&uri='+uri;
 }
 };
+//判断是清明节，加载css样式。
+function TombSweepingDay(){
+    var date=new Date();
+    if(date.getMonth()===3&&date.getDate()===4){
+        document.getElementsByTagName('head')[0].innerHTML += '<style type="text/css">html {filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);-webkit-filter: grayscale(1);}<style>'
+}
+    if(date.getMonth()===3&&date.getDate()===5){
+        document.getElementsByTagName('head')[0].innerHTML += '<style type="text/css">html {filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);-webkit-filter: grayscale(1);}<style>'
+}};
 //判断是国家公祭日12.13，加载css样式。
 function Memorial(){
     var date=new Date();
