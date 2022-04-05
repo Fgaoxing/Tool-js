780f00c2029e5f8bcc7c9ca576c178732f92ebc0
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-04-05 19:45:57 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -49,3 +49,15 @@ function lantern(){
         head.appendChild(styleNode);
     }
 })();
+
+// url参数获取
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return('/');
+}
