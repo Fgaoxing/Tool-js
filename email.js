8c794f372b0070757d073358931fd723ddce139b
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-04-07 22:30:17 +0800

    Update email.js

--- email.js
+++ email.js
@@ -2,17 +2,17 @@ var Email = {
 	send: function(a) {
 		return new Promise(function(n, e) {
 			a.nocache = Math.floor(1e6 * Math.random() + 1),
-			a.Action = "Send";
+			a.Action = 'Send';
 			var t = JSON.stringify(a);
-			Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t,
+			Email.ajaxPost('https://smtpjs.com/v3/smtpjs.aspx?', t,
 			function(e) {
 				n(e)
 			})
 		})
 	},
 	ajaxPost: function(e, n, t) {
-		var a = Email.createCORSRequest("POST", e);
-		a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"),
+		var a = Email.createCORSRequest('POST', e);
+		a.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'),
 		a.onload = function() {
 			var e = a.responseText;
 			null != t && t(e)
@@ -20,7 +20,7 @@ var Email = {
 		a.send(n)
 	},
 	ajax: function(e, n) {
-		var t = Email.createCORSRequest("GET", e);
+		var t = Email.createCORSRequest('GET', e);
 		t.onload = function() {
 			var e = t.responseText;
 			null != n && n(e)
@@ -29,7 +29,7 @@ var Email = {
 	},
 	createCORSRequest: function(e, n) {
 		var t = new XMLHttpRequest;
-		return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null,
+		return 'withCredentials' in t ? t.open(e, n, !0) : 'undefined' != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null,
 		t
 	}
 };
