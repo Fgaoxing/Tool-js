d13f24a2d8b7c6ee99a628a53390f6d1d570dbfd
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-04-08 14:43:53 +0800

    Update email.js

--- email.js
+++ email.js
@@ -33,16 +33,3 @@ var Email = {
 		t
 	}
 };
-function SendEmail(host,username,password,to,from,subject,body){
-	Email.send({
-		Host : host,
-		Username : username,
-		Password : password,
-		To : to,
-		From : from,
-		Subject : subject,
-		Body : body
-	}).then(
-  		message => write(message)
-	);
-}
